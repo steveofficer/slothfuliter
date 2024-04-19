d44ef2ca6c6638447780538d4e752afad3fa2d86
Steve <steve.officer@gmail.com>
2024-04-19 23:54:12 +0100

    More documentation

--- src/slothfulIterable.ts
+++ src/slothfulIterable.ts
@@ -18,7 +18,7 @@ export class SlothfulIterable<T> implements Iterable<T> {
      * @remarks
      * This function will immediately evaluate the sequence.
      * 
-     * @param predicate The function evaluate each item against.
+     * @param predicate The function to evaluate each item against.
      * @returns true if all items match the predicate, otherwise false.
      */
     every(predicate: (item: T) => boolean) {
@@ -35,7 +35,7 @@ export class SlothfulIterable<T> implements Iterable<T> {
      * Creates a new lazy sequence that contains only the items that match the provided predicate.
      * 
      * @remarks
-     * This function will not evaluate the sequence and will only apply when the sequence is iterated.
+     * This function will not immediately evaluate the sequence and will only apply when the sequence is iterated.
      * 
      * @param predicate The function evaluate each item against.
      * @returns a new sequence that when iterated contains only the items that matched the predicate.
@@ -70,7 +70,7 @@ export class SlothfulIterable<T> implements Iterable<T> {
      * Creates a new lazy sequence that contains items that have been transformed by a provided function.
      * 
      * @remarks
-     * This function will not evaluate the sequence and will only apply when the sequence is iterated.
+     * This function will not immediately evaluate the sequence and will only apply when the sequence is iterated.
      * 
      * @param f The transformation to apply against each item.
      * @returns a new sequence that when iterated contains the transformed items.
@@ -85,6 +85,16 @@ export class SlothfulIterable<T> implements Iterable<T> {
       return new SlothfulIterable<TResult>(() => iter(this.generator()))
     }
 
+    /**
+     * Iterates over each item the sequence and allows the item to be aggregated into a final result.
+     * 
+     * @remarks
+     * This function will immediately evaluate the sequence.
+     * 
+     * @param reducer A function that accepts the current result and the current item and returns a new result.
+     * @param initialValue The initial result to provide to the very first iteration.
+     * @returns The result produced by the final iteration.
+     */
     reduce<TResult>(reducer: (result: TResult, item: T) => TResult, initialValue: TResult) {
       let result = initialValue
       for (const item of this.generator()) {
@@ -94,6 +104,17 @@ export class SlothfulIterable<T> implements Iterable<T> {
       return result
     }
 
+    /**
+     * Operates similarly to {@link reduce}, however it produces the aggregated state after each item is
+     * iterated over.
+     * 
+     * @remarks
+     * This function will not immediately evaluate the sequence and will only apply when the sequence is iterated.
+     * 
+     * @param reducer A function that accepts the current result and the current item and returns a new result.
+     * @param initialState The initial result to provide to the very first iteration.
+     * @returns a new sequence that when iterated contains the running output of the {@link reducer}.
+     */
     scan<TResult>(reducer: (state: TResult, item: T) => TResult, initialState: TResult) {
       function * iter(iterator: Iterable<T>) {
         let state = initialState
@@ -106,6 +127,15 @@ export class SlothfulIterable<T> implements Iterable<T> {
       return new SlothfulIterable<TResult>(() => iter(this.generator()))
     }
 
+    /**
+     * Creates a new sequence that excludes the first {@link count} items
+     * 
+     * @remarks
+     * This function will not immediately evaluate the sequence and will only apply when the sequence is iterated.
+     * 
+     * @param count The number of items to skip over
+     * @returns a new sequence that exludes the first {@link count} items
+     */
     skip(count: number) {
       function * iter(iterator: Iterator<T>) {
         let counter = 0;
@@ -123,6 +153,15 @@ export class SlothfulIterable<T> implements Iterable<T> {
       return new SlothfulIterable(() => iter(this.generator()))
     }
   
+    /**
+     * Similar to {@link every} with the exception that it only requires a single item to match the predicate.
+     * 
+     * @remarks
+     * This function will immediately evaluate the sequence.
+     * 
+     * @param predicate The function to evaluate each item against.
+     * @returns true if at least one item matches the predicate, otherwise false.
+     */
     some(predicate: (item: T) => boolean) {
       for (const item of this.generator()) {
         if (predicate(item)) {
@@ -133,6 +172,15 @@ export class SlothfulIterable<T> implements Iterable<T> {
       return false
     }
 
+    /**
+     * Creates a new sequence that contains at most {@link count} items.
+     * 
+     * @remarks
+     * This function will not immediately evaluate the sequence and will only apply when the sequence is iterated.
+     * 
+     * @param count The number of items to return from the underlying sequence
+     * @returns a new sequence that contains at most {@link count} items
+     */
     take(count: number) {
       function * iter(iterator: Iterator<T>) {
         let counter = 0
@@ -147,6 +195,15 @@ export class SlothfulIterable<T> implements Iterable<T> {
       return new SlothfulIterable(() => iter(this.generator()))
     }
   
+    /**
+     * Creates a new sequence that terminates when the provided predicate is fulfilled.
+     * 
+     * @remarks
+     * This function will not immediately evaluate the sequence and will only apply when the sequence is iterated.
+     * 
+     * @param predicate The function to evaluate each item against.
+     * @returns a sequence that terminates when either the underlying sequence is done, or {@link predicate} is fulfilled.
+     */
     takeWhile(predicate: (item: T) => boolean) {
       function * iter(iterator: Iterator<T>) {
         let result = iterator.next()
@@ -159,11 +216,29 @@ export class SlothfulIterable<T> implements Iterable<T> {
       return new SlothfulIterable(() => iter(this.generator()))
     }
 
+    /**
+     * Iterates the underlying sequence once and then returns a new sequence based on the values obtained from the first iteration.
+     * 
+     * @remarks
+     * This function will immediately evaluate the sequence.
+     * 
+     * @returns a new sequence based on the values obtained from the immediate evaluation of the underlying sequence.
+     */
     cache() {
       const cachedIter = [...this.generator()]
       return new SlothfulIterable(() => cachedIter.values())
     }
 
+    /**
+     * Converts the underlying sequence to a Map
+     * 
+     * @remarks
+     * This function will immediately evaluate the sequence.
+     * 
+     * @param keySelector A function that returns the key for the Map given the current item from the sequence
+     * @param valueSelector A function that returns the value for the Map given the current item from the sequence
+     * @returns A Map that is derived from the underlying sequence.
+     */
     toMap<TKey, TValue>(keySelector: (item: T) => TKey, valueSelector: (item: T) => TValue) {
       let result = new Map<TKey, TValue>()
       for (const item of this.generator()) {
